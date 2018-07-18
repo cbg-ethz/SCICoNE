236936b459512af191f48c3501c15581af190a0e
anilbey <tuncel.manil@gmail.com>
2018-07-18 18:23:54 +0200

    commenting out debug attachment

--- src/Tree.h
+++ src/Tree.h
@@ -375,7 +375,7 @@ void Tree::recursive_copy(Node* source, Node *destination) {
 Node * Tree::prune_reattach() {
     // returns the pruned node (which is the attached node)
 
-    Node* prune_pos;
+    Node* prune_pos = nullptr;
     prune_pos = uniform_select(false);
 
     // copy all nodes
@@ -399,7 +399,7 @@ Node * Tree::prune_reattach() {
     rand_val = MathOp::random_uniform(1,destination_nodes.size());
     Node* attach_pos = nullptr;
     attach_pos = destination_nodes[rand_val -1];
-    attach_pos = all_nodes[5]; //TODO remove this after testing is complete
+    //attach_pos = all_nodes[5]; //TODO remove this after testing is complete
     //do not recompute you attach at the same pos
     if (prune_pos->parent != attach_pos)
     {
