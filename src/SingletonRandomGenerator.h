f025e77481c632370d0bae7ecf3c87706f51e9cc
anilbey <tuncel.manil@gmail.com>
2018-08-09 17:29:21 +0200

    singleton random generator

---
+++ src/SingletonRandomGenerator.h
@@ -0,0 +1,37 @@
+//
+// Created by Tuncel  Mustafa Anil on 8/9/18.
+//
+
+#ifndef SC_DNA_SINGLETONRANDOMGENERATOR_H
+#define SC_DNA_SINGLETONRANDOMGENERATOR_H
+
+#include <random>
+
+class SingletonRandomGenerator
+{
+public:
+
+
+    std::mt19937 generator; //Standard mersenne_twister_engine
+
+    static std::mt19937& get_generator()
+    {
+        static SingletonRandomGenerator    instance; // Guaranteed to be destroyed.
+        // Instantiated on first use.
+        return instance.generator;
+    }
+private:
+    SingletonRandomGenerator() {
+        std::random_device rd;  //Will be used to obtain a seed for the random number engine
+        std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
+        generator = gen;
+    }
+
+public:
+    SingletonRandomGenerator(SingletonRandomGenerator const&) = delete;
+    void operator=(SingletonRandomGenerator const&)  = delete;
+
+};
+
+
+#endif //SC_DNA_SINGLETONRANDOMGENERATOR_H
