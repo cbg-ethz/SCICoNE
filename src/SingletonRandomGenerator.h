8a7b9b6d064ae7eba179843a2ef00581a89a0199
anilbey <tuncel.manil@gmail.com>
2018-08-09 17:37:23 +0200

    introducing a seed

--- src/SingletonRandomGenerator.h
+++ src/SingletonRandomGenerator.h
@@ -14,17 +14,27 @@ public:
 
     std::mt19937 generator; //Standard mersenne_twister_engine
 
-    static std::mt19937& get_generator()
+    static std::mt19937& get_generator(int seed=-1)
     {
-        static SingletonRandomGenerator    instance; // Guaranteed to be destroyed.
+        static SingletonRandomGenerator instance(seed); // Guaranteed to be destroyed.
         // Instantiated on first use.
         return instance.generator;
     }
 private:
-    SingletonRandomGenerator() {
-        std::random_device rd;  //Will be used to obtain a seed for the random number engine
-        std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
-        generator = gen;
+    SingletonRandomGenerator(int seed) {
+
+        if (seed == -1)
+        {
+            std::random_device rd;  //Will be used to obtain a seed for the random number engine
+            std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
+            generator = gen;
+        }
+        else
+        {
+            std::mt19937 gen(seed);
+            generator = gen;
+        }
+
     }
 
 public:
