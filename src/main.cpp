2238d982328a9f635592b821385a25cdf4fe55c7
anilbey <tuncel.manil@gmail.com>
2018-09-10 15:25:09 +0200

    computation of s_p vector for breakpoint detection

--- src/main.cpp
+++ src/main.cpp
@@ -46,7 +46,18 @@ vector<vector<double>> read_counts(const string path)
     return mat;
 }
 
-void disp_vec(vector<vector<double>> vec) {
+void disp_vec(vector<vector<long double>>& vec) {
+/*
+ * displays the long double vector
+ * */
+
+    for (auto const &v1: vec) {
+        for (auto const &v2: v1)
+            cout << v2 << ' ';
+        cout << endl;
+    }
+}
+void disp_vec(vector<vector<double>>& vec) {
 /*
  * displays the double vector
  * */
@@ -80,7 +91,7 @@ int main() {
     // mcmc.random_initialize();
     mcmc.compute_t_table(D,r);
 
-    mcmc.infer_mcmc(D, r, move_probs, 50000);
+    mcmc.infer_mcmc(D, r, move_probs, 5000);
     mcmc.write_best_tree();
 
     mcmc.destroy();
@@ -95,68 +106,64 @@ int main() {
     u_int window_size = 1;
     vector<vector<double>> aic_vec = MathOp::likelihood_ratio(mat,window_size);
 
-
-    // dynamic programming
-//    for (auto const &v2: aic_vec[11])
-//        cout << v2 << ' ';
-//    cout << endl;
-
-    //auto n_aic_vec = log_normalize(aic_vec[0]);
     vector<vector<double>> sigma;
-    vector<double> log_priors;
+
     int n_breakpoints = aic_vec.size();
     int n_cells = aic_vec[0].size();
     cout <<"n_breakpoints: " << n_breakpoints << " n_cells: " << n_cells <<endl;
-    int i = 0;
-    for (auto vec: aic_vec) // compute sigma matrix
+
+    for (auto &vec: aic_vec) // compute sigma matrix
     {
-        // for each breakpoint
-//        cout << i++ <<" --> ";
         auto res = MathOp::combine_scores(vec);
         sigma.push_back(res);
-//        for (auto const &v2: res)
-//            cout << v2 << ' ';
-//        cout <<endl;
-
     }
 
+    vector<double> log_priors;
     for (int j = 0; j < n_cells; ++j) {
         log_priors.push_back(MathOp::breakpoint_log_prior(j, n_cells,0.5));
     }
 
 
-    vector<vector<double>> log_posterior;
+    vector<vector<long double>> log_posterior;
 
     for (int k = 0; k < n_breakpoints; ++k) {
-        log_posterior.push_back(vector<double>());
+        log_posterior.push_back(vector<long double>());
         for (int j = 0; j < n_cells; ++j) {
-            double val = log_priors[j] + sigma[k][j];
+            long double val = log_priors[j] + sigma[k][j];
             log_posterior[k].push_back(val);
         }
     }
 
-    // convert to real space
-    for (int k = 0; k < n_breakpoints; ++k) {
-        double max_log_posterior = *max_element(log_posterior[k].begin(), log_posterior[k].end());
-        for (int j = 0; j < n_cells; ++j) {
-            double val = exp(log_posterior[k][j] - max_log_posterior);
-            log_posterior[k][j] = val;
-        }
-    }
+    cout<<"log posterior: \n";
+    disp_vec(log_posterior);
 
 
-
+    vector<vector<long double>> posterior;
     int k_star = 4;
 
-    vector<double> s_p;
-
+    vector<long double> s_p;
     for (int l = 0; l < n_breakpoints; ++l)
     {
-        double sp_num = std::accumulate(log_posterior[l].begin(), log_posterior[l].begin()+k_star-1, 0.0);
-        double sp_denom = std::accumulate(log_posterior[l].begin(), log_posterior[l].end(), 0.0);
-        double fraction = sp_num / sp_denom;
+        posterior.push_back(vector<long double>());
 
-        s_p.push_back(log(1 -  (sp_num) / (sp_denom)));
+        long double max_num = *max_element(log_posterior[l].begin(), log_posterior[l].begin()+k_star-1);
+        long double max_denom = *max_element(log_posterior[l].begin(), log_posterior[l].end());
+
+        for (int j = 0; j < k_star - 1; ++j) {
+            long double val =exp(log_posterior[l][j] - max_num);
+            posterior[l].push_back(val);
+        }
+        for (int k = k_star -1 ; k < log_posterior[l].size(); ++k) {
+            long double val =exp(log_posterior[l][k] - max_denom);
+            posterior[l].push_back(val);
+        }
+
+
+        long double sp_num = std::accumulate(posterior[l].begin(), posterior[l].begin()+k_star-1, 0.0);
+        long double sp_denom = std::accumulate(posterior[l].begin(), posterior[l].end(), 0.0);
+        long double fraction = sp_num / sp_denom;
+
+        s_p.push_back(-1*log( 1- (1-fraction)));
     }
 
     std::ofstream output_file("./s_p.txt");
