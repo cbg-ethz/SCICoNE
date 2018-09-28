ee1d0f3d18d3971ea08cef9170c5df2084f8fbac
anilbey <tuncel.manil@gmail.com>
2018-09-28 18:22:00 +0200

    number of repetitions argument

--- simulations/run_simulations.cpp
+++ simulations/run_simulations.cpp
@@ -22,6 +22,7 @@ int main(int argc, char* argv[])
     int n_cells = 500;
     int n_reads = 10000;
     int n_iters = 5000;
+    int n_repetitions = 100;
     int max_region_size = 10;
     int ploidy = 2;
 
@@ -30,6 +31,7 @@ int main(int argc, char* argv[])
     options.add_options()
             ("n_regions", "Number of regions", cxxopts::value(n_regions))
             ("n_iters", "Number of iterations", cxxopts::value(n_iters))
+            ("n_rep", "Number of repetitions", cxxopts::value(n_repetitions))
             ("n_reads", "Number of reads", cxxopts::value(n_reads));
 
     auto result = options.parse(argc, argv);
@@ -46,6 +48,10 @@ int main(int argc, char* argv[])
     {
         n_iters = result["n_iters"].as<int>();
     }
+    if (result.count("n_rep"))
+    {
+        n_repetitions = result["n_rep"].as<int>();
+    }
 
 
 
@@ -57,11 +63,15 @@ int main(int argc, char* argv[])
     Simulation sim(n_regions, n_nodes, lambda_r, lambda_c, n_cells, n_reads, max_region_size, ploidy);
     double delta_random_init = sim.random_cnvs_inference();
 
-    sim.infer_cnvs(n_iters); // n_iters: 50000
-    cout << "delta from our method: " << sim.delta_vec[0] << endl;
+
+    for (int i = 0; i < n_repetitions; ++i)
+    {
+        sim.infer_cnvs(n_iters); // n_iters: 50000
+        cout << "delta from our method: " << sim.delta_vec[i] << endl;
+    }
+
     cout << "delta from random method: " << delta_random_init << endl;
 
-    cout <<'d';
     //simulate(n_regions, n_nodes, lambda_r, lambda_c, n_cells, n_reads, D, region_sizes); // initializes D and region_sizes
 
     // initialize the tree and infer the CNV profiles
