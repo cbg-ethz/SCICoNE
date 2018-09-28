0ad15fbf6f12bd7d43ea533b591ec061ca83a565
anilbey <tuncel.manil@gmail.com>
2018-09-28 15:27:49 +0200

    simulations arguments parsing with cxxopts

--- simulations/run_simulations.cpp
+++ simulations/run_simulations.cpp
@@ -3,7 +3,9 @@
 //
 
 #include <iostream>
+#include <string>
 #include "Simulation.h"
+#include <cxxopts.hpp>
 
 using namespace std;
 
@@ -12,14 +14,40 @@ using namespace std;
 int main(int argc, char* argv[])
 {
 
+    // the default values
     int n_regions = 50;
     int n_nodes = 50;
     double lambda_r = 0.1;
     double lambda_c = 0.2;
     int n_cells = 500;
     int n_reads = 10000;
+    int n_iters = 5000;
     int max_region_size = 10;
     int ploidy = 2;
+
+
+    cxxopts::Options options("Mcmc simulations", "simulates cnv values, infers them and benchmarks");
+    options.add_options()
+            ("n_regions", "Number of regions", cxxopts::value(n_regions))
+            ("n_iters", "Number of iterations", cxxopts::value(n_iters))
+            ("n_reads", "Number of reads", cxxopts::value(n_reads));
+
+    auto result = options.parse(argc, argv);
+
+    if (result.count("n_regions"))
+    {
+        n_regions = result["n_regions"].as<int>();
+    }
+    if (result.count("n_reads"))
+    {
+        n_reads = result["n_reads"].as<int>();
+    }
+    if (result.count("n_iters"))
+    {
+        n_iters = result["n_iters"].as<int>();
+    }
+
+
 
     // create the D matrix
     vector<vector<double>> D(n_cells, vector<double>(n_regions)); // initialize with the default value
@@ -29,7 +57,7 @@ int main(int argc, char* argv[])
     Simulation sim(n_regions, n_nodes, lambda_r, lambda_c, n_cells, n_reads, max_region_size, ploidy);
     double delta_random_init = sim.random_cnvs_inference();
 
-    sim.infer_cnvs(1500); // n_iters: 50000
+    sim.infer_cnvs(n_iters); // n_iters: 50000
     cout << "delta from our method: " << sim.delta_vec[0] << endl;
     cout << "delta from random method: " << delta_random_init << endl;
 
