eb03db2636d916775d71b6ed85caa6535ff796c8
Mustafa Anil Tuncel <tuncel.manil@gmail.com>
2018-10-17 16:13:45 +0200

    [simulations] added variable n_nodes

--- workflows/self_benchmark_snakefile.py
+++ workflows/self_benchmark_snakefile.py
@@ -7,7 +7,7 @@ configfile: "self_benchmark_config.json"
 parameters
 '''
 
-n_nodes = 10
+n_nodes = [10,20,40]
 n_regions = [n_nodes,2*n_nodes,4*n_nodes]
 n_bins = 10000
 n_reads = [1000, 10000, 100000]
@@ -16,21 +16,17 @@ n_cells = 500
 n_iters = 1000000 # 1 million iters for each setting
 
 
-# TODO: have rules to compile the code
-
 SIM_OUTPUT= "/cluster/work/bewi/members/tuncel/data/dna/recomb_simulations" # "simulations_output"
 
-# time = datetime.datetime.now().strftime("%Y-%m-%d_%H_%M")
-time = "9_sims"
+prefix = "recomb_simulations"
 
 '''
 rules
 '''
-# sample input: 50regions_10000reads_1538315330937578_deltas.csv
 
 rule all:
     input:
-        read_region_sims = expand(SIM_OUTPUT + '_' + time +'/'+'{regions}'+'regions_'+ '{reads}'+'reads'+ '/'+ '{rep_id}' +'_deltas.csv', regions=n_regions,reads=n_reads, rep_id=[x for x in range(0,n_repetitions)])
+        read_region_sims = expand(SIM_OUTPUT + '_' + prefix +'/'+ '{nodes}' + 'nodes_'  + '{regions}'+'regions_'+ '{reads}'+'reads'+ '/'+ '{rep_id}' +'_deltas.csv', nodes=n_nodes, regions=n_regions,reads=n_reads, rep_id=[x for x in range(0,n_repetitions)])
     output:
     shell:
 	    "echo STATUS:SUCCESS. All of the rules are ran through."
@@ -49,10 +45,9 @@ rule run_sim:
     threads:
         config["cnv_trees"]["threads"]
     output:
-        # files = rules.all.input.read_region_sims
-        SIM_OUTPUT+ '_' + time +'/'+'{regions}'+'regions_'+ '{reads}'+'reads'+ '/' + '{rep_id}' + '_deltas.csv'
+        SIM_OUTPUT+ '_' + prefix +'/'+ '{nodes}' + 'nodes_' + '{regions}'+'regions_'+ '{reads}'+'reads'+ '/' + '{rep_id}' + '_deltas.csv'
     shell:
-        "{params.sim_bin} --n_regions {wildcards.regions} --n_reads {wildcards.reads} --n_iters {params.n_iters} --n_cells {params.n_cells} --n_bins {params.n_bins} --n_nodes {params.n_nodes} --n_rep 1 --verbosity 0 --ploidy 2 --postfix {wildcards.rep_id}; mv {wildcards.regions}regions_{wildcards.reads}reads_{wildcards.rep_id}_deltas.csv {output}"
+        "{params.sim_bin} --n_regions {wildcards.regions} --nodes {wildcards.nodes}  --n_reads {wildcards.reads} --n_iters {params.n_iters} --n_cells {params.n_cells} --n_bins {params.n_bins} --n_nodes {params.n_nodes} --n_rep 1 --verbosity 0 --ploidy 2 --postfix {wildcards.rep_id}; mv {wildcards.nodes}nodes_{wildcards.regions}regions_{wildcards.reads}reads_{wildcards.rep_id}_deltas.csv {output}"
 
 
 
