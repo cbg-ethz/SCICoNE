959040268651a3bad5d937369216262ea2041951
anilbey <tuncel.manil@gmail.com>
2018-12-06 23:20:55 +0100

    added StatsLib

---
+++ include/stats/stats.hpp
@@ -0,0 +1,40 @@
+/*################################################################################
+  ##
+  ##   Copyright (C) 2011-2018 Keith O'Hara
+  ##
+  ##   This file is part of the StatsLib C++ library.
+  ##
+  ##   Licensed under the Apache License, Version 2.0 (the "License");
+  ##   you may not use this file except in compliance with the License.
+  ##   You may obtain a copy of the License at
+  ##
+  ##       http://www.apache.org/licenses/LICENSE-2.0
+  ##
+  ##   Unless required by applicable law or agreed to in writing, software
+  ##   distributed under the License is distributed on an "AS IS" BASIS,
+  ##   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  ##   See the License for the specific language governing permissions and
+  ##   limitations under the License.
+  ##
+  ################################################################################*/
+
+#ifndef _statslib_HPP
+#define _statslib_HPP
+
+#include "math/gcem.hpp"
+#include "misc/statslib_options.hpp"
+
+namespace stats
+{
+    #include "misc/matrix_ops.hpp"
+
+    #include "dens/dens.hpp"
+
+    #include "prob/prob.hpp"
+
+    #include "quant/quant.hpp"
+
+    #include "rand/rand.hpp"
+}
+
+#endif
