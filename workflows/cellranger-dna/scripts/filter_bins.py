4ff0fa5f00470563420ac74ada11410cbb10c33b
anilbey <tuncel.manil@gmail.com>
2019-01-18 15:10:30 +0100

    [workflow-cellranger-dna] providing the bins_genome output after filtering

--- workflows/cellranger-dna/scripts/filter_bins.py
+++ workflows/cellranger-dna/scripts/filter_bins.py
@@ -37,20 +37,29 @@ h5f = h5py.File(args.hdf5, 'r')
 
 mat = merge_chromosomes(h5f)
 
-all_chromosomes = list(h5f['cnvs'].keys())
-# list of all cnv arrays
-chr_sizes = []
+bin_size = h5f["constants"]["bin_size"][()]
+n_bins = mat.shape[1]
+bin_ids = [x for x in range(0,n_bins)]
+bin_df = pd.DataFrame(bin_ids, columns=["bin_ids"])
 
-for chr in all_chromosomes:
-    chr_sizes.append(h5f['cnvs'][chr][:].shape[1])
+bin_df["start"] = bin_df["bin_ids"] * bin_size
+bin_df["end"] = bin_df["start"] + bin_size
+print(bin_df.head())
 
-all_chr_ids = []
-for i in range(0,len(chr_sizes)):
-    all_chr_ids += [all_chromosomes[i]]*chr_sizes[i]
+# all_chromosomes = list(h5f['cnvs'].keys())
+# # list of all cnv arrays
+# chr_sizes = []
+#
+# for chr in all_chromosomes:
+#     chr_sizes.append(h5f['cnvs'][chr][:].shape[1])
+#
+# all_chr_ids = []
+# for i in range(0,len(chr_sizes)):
+#     all_chr_ids += [all_chromosomes[i]]*chr_sizes[i]
 
 
 if args.bins is None:
-    print("no bins are provided")
+    print("no bins to exclude are provided")
 else:
     print(args.bins)
 
@@ -65,10 +74,16 @@ print(sum(negative_bins_filter_mask))
 print("matrix shape before & after filtering for negatives")
 print(mat.shape)
 mat = mat[:,negative_bins_filter_mask]
-
 print(mat.shape)
 
+print("bin_df shape before & after filtering for negatives")
+print(bin_df.shape)
+bin_df = bin_df[negative_bins_filter_mask]
+print(bin_df.shape)
+
 np.savetxt(args.output_path + '/' + args.sample_name +"_filtered_counts.tsv", mat, delimiter='\t')
+
+bin_df.to_csv(args.output_path + '/' + args.sample_name + "_bins_genome.tsv",sep='\t',index=False, header=False)
 
 print("Output written to: " + args.output_path)
 
