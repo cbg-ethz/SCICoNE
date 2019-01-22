cfdbd6041e0bbdfa13010bb410be50391151cfd2
anilbey <tuncel.manil@gmail.com>
2019-01-22 15:08:23 +0100

    [notebook] computing the ground truth from cell genotypes

--- workflows/bp_detection_performance/bp_snakefile.py
+++ workflows/bp_detection_performance/bp_snakefile.py
@@ -14,7 +14,7 @@ window_size =  config['params']['window_size']
 rule all:
     input:
         bps_comparison = expand(SIM_OUTPUT + '{dataset_id}' + '_all_bps_comparison.csv', dataset_id=sim_data),
-        ground_truth = expand(SIM_OUTPUT + str(n_nodes) + 'nodes_' + str(n_regions) + 'regions_' + str(n_reads) + 'reads_' + '{dataset_id}' + '_effective_regions.txt', dataset_id=sim_data)
+        ground_truth = expand(SIM_OUTPUT + str(n_nodes) + 'nodes_' + str(n_regions) + 'regions_' + str(n_reads) + 'reads_' + '{dataset_id}' + '_ground_truth.txt', dataset_id=sim_data)
     output:
     shell:
         "echo Successfully generated all of the data."
@@ -29,16 +29,16 @@ rule simulate:
         n_reads = n_reads,
         n_cells = n_cells,
         dmatrix_name = str(n_nodes) + 'nodes_' + str(n_regions) + 'regions_' + str(n_reads) + 'reads_' + '{dataset_id}_d_mat.txt',
-        effective_regions_name = str(n_nodes) + 'nodes_' + str(n_regions) + 'regions_' + str(n_reads) + 'reads_' + '{dataset_id}_effective_regions.txt'
+        ground_truth_name = str(n_nodes) + 'nodes_' + str(n_regions) + 'regions_' + str(n_reads) + 'reads_' + '{dataset_id}_ground_truth.txt'
     input:
     output:
         dmatrix = SIM_OUTPUT + str(n_nodes) + 'nodes_' + str(n_regions) + 'regions_' + str(n_reads) + 'reads_' + '{dataset_id}_d_mat.txt',
-        effective_regions = SIM_OUTPUT + str(n_nodes) + 'nodes_' + str(n_regions) + 'regions_' + str(n_reads) + 'reads_' + '{dataset_id}_effective_regions.txt'
+        ground_truth = SIM_OUTPUT + str(n_nodes) + 'nodes_' + str(n_regions) + 'regions_' + str(n_reads) + 'reads_' + '{dataset_id}_ground_truth.txt'
         # 10nodes_40regions_10000reads_sim_tiny2_d_mat.txt
     shell:
         "{params.binary} --n_bins {params.n_bins} --n_regions {params.n_regions} --n_nodes {params.n_nodes} --n_reads {params.n_reads} \
         --n_cells {params.n_cells} --postfix {wildcards.dataset_id} --verbosity 2 --ploidy 2; mv {params.dmatrix_name} {output.dmatrix}; \
-        mv {params.effective_regions_name} {output.effective_regions}"
+        mv {params.ground_truth_name} {output.ground_truth}"
 
 
 rule bp_detect:
@@ -56,5 +56,3 @@ rule bp_detect:
         "{params.binary} --n_bins {params.n_bins} --n_cells {params.n_cells} --window_size {params.window_size} \
                 --threshold 1 --verbosity 3 --postfix {wildcards.dataset_id} --d_matrix_file {input.dmatrix}; \
                 mv {params.bps_comparisons_name} {output}"
-
-
