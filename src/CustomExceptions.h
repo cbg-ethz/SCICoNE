9bbfb2e021032a1eeeaed913cf66ea8ecec8f804
anilbey <tuncel.manil@gmail.com>
2019-08-23 14:13:41 +0200

    CustomExceptions: use noexcept instead of throw()

--- src/CustomExceptions.h
+++ src/CustomExceptions.h
@@ -6,51 +6,9 @@
 #define SC_DNA_CUSTOMEXCEPTIONS_H
 
 #include <stdexcept>
+#include <utility>
 
 #endif //SC_DNA_CUSTOMEXCEPTIONS_H
-
-class NotImplementedException: public std::exception
-{
-    // Based on user 992460's answer on stackoverflow.com/questions/8152720/
-
-public:
-    /** Constructor (C strings).
-     *  @param message C-style string error message.
-     *                 The string contents are copied upon construction.
-     *                 Hence, responsibility for deleting the char* lies
-     *                 with the caller.
-     */
-    explicit NotImplementedException(const char* message):
-            msg_(message)
-    {
-    }
-
-    /** Constructor (C++ STL strings).
-     *  @param message The error message.
-     */
-    explicit NotImplementedException(const std::string& message):
-            msg_(message)
-    {}
-
-    /** Destructor.
-     * Virtual to allow for subclassing.
-     */
-    virtual ~NotImplementedException() throw (){}
-
-    /** Returns a pointer to the (constant) error description.
-     *  @return A pointer to a const char*. The underlying memory
-     *          is in posession of the Exception object. Callers must
-     *          not attempt to free the memory.
-     */
-    virtual const char* what() const throw (){
-        return msg_.c_str();
-    }
-
-protected:
-    /** Error message.
-     */
-    std::string msg_;
-};
 
 class InvalidMove: public std::exception
 {
@@ -72,3 +30,47 @@ public:
 protected:
     std::string msg_;
 };
+
+class NotImplementedException: public std::exception
+{
+    // Based on user 992460's answer on stackoverflow.com/questions/8152720/
+
+public:
+    /** Constructor (C strings).
+     *  @param message C-style string error message.
+     *                 The string contents are copied upon construction.
+     *                 Hence, responsibility for deleting the char* lies
+     *                 with the caller.
+     */
+    explicit NotImplementedException(const char* message):
+            msg_(message)
+    {
+    }
+
+    /** Constructor (C++ STL strings).
+     *  @param message The error message.
+     */
+    explicit NotImplementedException(std::string  message):
+            msg_(std::move(message))
+    {}
+
+    /** Destructor.
+     * Virtual to allow for subclassing.
+     */
+    ~NotImplementedException() noexcept override= default;
+
+    /** Returns a pointer to the (constant) error description.
+     *  @return A pointer to a const char*. The underlying memory
+     *          is in posession of the Exception object. Callers must
+     *          not attempt to free the memory.
+     */
+    const char* what() const noexcept override{
+        return msg_.c_str();
+    }
+
+protected:
+    /** Error message.
+     */
+    std::string msg_;
+};
+
