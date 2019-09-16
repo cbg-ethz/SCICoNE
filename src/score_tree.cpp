c00fef07049afa2e9febb82c50619314f2569676
Mustafa Anil Tuncel <tuncel.manil@gmail.com>
2019-09-16 15:21:53 +0200

    score tree executable, input is per regions

--- src/score_tree.cpp
+++ src/score_tree.cpp
@@ -38,8 +38,7 @@ int main( int argc, char* argv[]) {
     copy_number_limit = 5;
     is_overdispersed = 1;
     int n_cells;
-    int n_bins = 10000;
-    size_t n_regions;
+    int n_regions;
     int ploidy=2;
     int verbosity=0;
     string file;
@@ -53,7 +52,7 @@ int main( int argc, char* argv[]) {
     options.add_options()
             ("region_sizes_file", "Path to the file containing the region sizes, each line contains one region size", cxxopts::value(region_sizes_file))
             ("d_matrix_file", "Path to the counts matrix file, delimiter: ',', line separator: '\n' ", cxxopts::value(d_matrix_file))
-            ("n_bins", "Number of bins in the input matrix", cxxopts::value(n_bins))
+            ("n_regions", "Number of regions in the input matrix", cxxopts::value(n_regions))
             ("postfix", "Postfix to be added to the output files, this is useful when you are running multiple simulations through a work flow management system", cxxopts::value(f_name_postfix))
             ("n_cells", "Number of cells in the input matrix", cxxopts::value(n_cells))
             ("print_precision", "the precision of the score printing", cxxopts::value(print_precision))
@@ -64,18 +63,14 @@ int main( int argc, char* argv[]) {
             ;
     auto result = options.parse(argc, argv);
 
+    std::cout << "Reading the input matrix..." << std::endl;
+    vector<vector<double>> d_regions(n_cells, vector<double>(n_regions));
+    Utils::read_counts(d_regions, d_matrix_file);
 
-    // read the input d_matrix
-    vector<vector<double>> d_bins(n_cells, vector<double>(n_bins));
-    Utils::read_counts(d_bins, d_matrix_file);
-
-    // read the region_sizes file
+    std::cout << "Reading the region_sizes file..." << std::endl;
     vector<int> region_sizes;
-    vector<vector<double>> d_regions;
-
     Utils::read_vector(region_sizes, region_sizes_file);
-    // Merge the bins into regions
-    d_regions = Utils::condense_matrix(d_bins, region_sizes);
+
     n_regions = region_sizes.size();
 
     Inference mcmc(n_regions, ploidy, verbosity);
