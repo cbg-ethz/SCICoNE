878c1d45ff13f8773b0e672a3b8bc00bb50535bc
anilbey <tuncel.manil@gmail.com>
2019-10-08 17:42:50 +0200

    simulations: add nu as an argument

--- simulations/run_simulations.cpp
+++ simulations/run_simulations.cpp
@@ -45,6 +45,7 @@ int main(int argc, char* argv[]) {
     copy_number_limit = 15;
     is_overdispersed = 0;
     eta = 1e-4;
+    double nu = 1.0;
 
     // minimum region size should be bigger than window_size
     unsigned min_region_size = 10;
@@ -68,6 +69,7 @@ int main(int argc, char* argv[]) {
             ("copy_number_limit", "the maximum copy number profile one bin or region can have", cxxopts::value(copy_number_limit))
             ("min_reg_size", "the minimum size that a region can have", cxxopts::value(min_region_size))
             ("c_penalise","term that penalises trees containing cancelling events to be added to tree event prior",cxxopts::value(c_penalise))
+            ("nu","nu parameter, the overdispersion variable",cxxopts::value(nu))
             ;
 
     auto result = options.parse(argc, argv);
@@ -79,6 +81,10 @@ int main(int argc, char* argv[]) {
         //set a seed number for reproducibility
         SingletonRandomGenerator::get_instance(seed);
     }
+    if(result.count("nu"))
+        is_overdispersed = 1;
+    else
+        is_overdispersed = 0;
 
     Simulation sim(n_regions, n_bins, n_nodes, n_cells, n_reads, max_region_size, ploidy, verbosity);
 
