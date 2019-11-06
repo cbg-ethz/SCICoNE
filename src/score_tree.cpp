46796138ec380850c410194b4f1e4d674287d12d
pedrofale <pedro.ferreira@bsse.ethz.ch>
2019-11-06 01:51:52 +0100

    Use cluster sizes in main

--- src/score_tree.cpp
+++ src/score_tree.cpp
@@ -47,6 +47,7 @@ int main( int argc, char* argv[]) {
     string file;
     string region_sizes_file = "";
     string d_matrix_file = "";
+    string cluster_sizes_file = "";
     string f_name_postfix = ""; //posfix
     double nu = 1.0;
     eta = 1e-4;
@@ -55,6 +56,7 @@ int main( int argc, char* argv[]) {
     options.add_options()
             ("region_sizes_file", "Path to the file containing the region sizes, each line contains one region size", cxxopts::value(region_sizes_file))
             ("d_matrix_file", "Path to the counts matrix file, delimiter: ',', line separator: '\n' ", cxxopts::value(d_matrix_file))
+            ("cluster_sizes_file", "Path to the file containing the cluster sizes, each line contains one cluster size", cxxopts::value(cluster_sizes_file))
             ("n_regions", "Number of regions in the input matrix", cxxopts::value(n_regions))
             ("postfix", "Postfix to be added to the output files, this is useful when you are running multiple simulations through a work flow management system", cxxopts::value(f_name_postfix))
             ("n_cells", "Number of cells in the input matrix", cxxopts::value(n_cells))
@@ -77,12 +79,27 @@ int main( int argc, char* argv[]) {
 
     n_regions = region_sizes.size();
 
+    vector<int> cluster_sizes;
+    if (result.count("cluster_sizes_file"))
+    {
+      std::cout << "Reading the cluster_sizes file..." << std::endl;
+      Utils::read_vector(cluster_sizes, cluster_sizes_file);
+    }
+    else
+    {
+      if (n_cells < 20)
+        std::cout << "Warning: there are only " << n_cells <<  " observations. If these are clusters, the cluster_sizes_file parameter should be specified for accurate tree scoring.";
+
+      for (int i=0;i<n_cells;i++)
+        cluster_sizes.push_back(1);
+    }
+
     Inference mcmc(n_regions, ploidy, verbosity);
     mcmc.initialize_from_file(file);
     mcmc.t.nu = nu;
     //mcmc.compute_neutral_table(d_regions, region_sizes);
-    mcmc.compute_t_table(d_regions,region_sizes);
-    mcmc.compute_t_od_scores(d_regions, region_sizes);
+    mcmc.compute_t_table(d_regions,region_sizes,cluster_sizes);
+    mcmc.compute_t_od_scores(d_regions, region_sizes,cluster_sizes);
     mcmc.update_t_prime(); // set t_prime to t
 
     // write the tree
