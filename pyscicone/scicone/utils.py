dea8917fc697b05d4ea23f92ede10d0f7b4104c0
pedrofale <pedro.ferreira@bsse.ethz.ch>
2020-05-22 15:25:59 +0100

    Add function to sort (potentially incomplete) list of chromosomes

--- pyscicone/scicone/utils.py
+++ pyscicone/scicone/utils.py
@@ -1,4 +1,28 @@
+import numpy as np
 from copy import deepcopy
+
+def sort_chromosomes(chromosome_list):
+    """
+    Sorts a list of unordered chromosome names
+    :param chromosome_list: list of unordered characters denoting chromosomes '1', '2', ..., 'X', 'Y'
+    """
+    # Replace X and Y with 23 and 24
+    sorted_chromosome_list = np.array(chromosome_list)
+    sorted_chromosome_list[np.where(sorted_chromosome_list == "X")[0]] = 23
+    sorted_chromosome_list[np.where(sorted_chromosome_list == "Y")[0]] = 24
+
+    # Convert everything to integer
+    sorted_chromosome_list = sorted_chromosome_list.astype(int)
+
+    # Sort
+    sorted_chromosome_list = np.sort(sorted_chromosome_list)
+
+    # Convert back to string
+    sorted_chromosome_list = sorted_chromosome_list.astype(str)
+    sorted_chromosome_list[np.where(sorted_chromosome_list == "23")[0]] = "X"
+    sorted_chromosome_list[np.where(sorted_chromosome_list == "24")[0]] = "Y"
+
+    return sorted_chromosome_list
 
 def create_fusion_tree(learned_tree, region_neutral_states):
     fusion_tree = deepcopy(learned_tree)
