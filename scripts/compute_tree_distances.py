ba90533e2bb143087dbfe40269eb965ccdc2590d
pedrofale <pedro.ferreira@bsse.ethz.ch>
2022-02-18 17:24:17 +0100

    Compute tree-tree distances as fraction of cells with less than some number of typical events

--- scripts/compute_tree_distances.py
+++ scripts/compute_tree_distances.py
@@ -1,18 +1,23 @@
-# Usage: compute_tree_distances.py --pdist_files pdist1.txt,pdist2.txt,...,pdistN.txt
+# Usage: compute_tree_distances.py pdist1.txt,pdist2.txt,...,pdistN.txt [--max_dist 1]
 
 import argparse 
 import numpy as np
 
 parser = argparse.ArgumentParser()
 parser.add_argument('pdist_files')
+parser.add_argument('--max_dist', default=1)
 args = parser.parse_args()
 
 pdist_files = args.pdist_files.split(',')
 n_trees = len(pdist_files)
+max_dist = args.max_dist
 
-def tree_dist(pdist1, pdist2):
+def tree_dist(pdist1, pdist2, max_dist=1):
     n_cells = pdist1.shape[0]
-    return np.linalg.norm(pdist1 - pdist2) / np.sqrt(n_cells*(n_cells-1)/2)
+
+    # Fraction of cells with distance smaller than max_dist
+    dists = np.abs(pdist1-pdist2)
+    return np.sum(dists[-1] < max_dist) / n_cells
 
 # Load cell-cell distance matrices
 pcdists = []
@@ -23,7 +28,8 @@ for pdist_file in pdist_files:
 ptdists = [] 
 for i in range(n_trees):
     for j in range(i):
-        ptdists.append(tree_dist(pcdists[i], pcdists[j]))
+        ptdists.append(tree_dist(pcdists[i], pcdists[j], max_dist))
+
 
 # Compute average 
 print(np.mean(ptdists))
